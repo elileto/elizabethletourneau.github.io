cb0698e850f577d237fc754b64290bbeb0955240
Elizabeth Letourneau <elizabeth.letourneau@shopify.com>
2018-06-01 09:39:26 -0400

    queermafia site

---
+++ app/booster.js
@@ -0,0 +1,14 @@
+
+
+class Booster {
+  constructor(name, multiplier, cost, timer){
+    this.activated = false;
+    this.available = true;
+    this.name = name;
+    this.multiplier = multiplier;
+    this.cost = cost;
+    this.timer = timer;
+  }
+
+}
+module.exports = Booster;
